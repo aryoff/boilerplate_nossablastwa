f99568da69bb5a1fc301ca658b6e42b75f5d621f
Aryo Fajar <aryoff@gmail.com>
2022-09-12 16:09:17 +0700

    hide menu

--- Resources/views/layouts/menuitems.blade.php
+++ Resources/views/layouts/menuitems.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item has-treeview">
+<li class="nav-item has-treeview" style="display: none;">
   <a href="#" class="nav-link">
     <i class="nav-icon fas fa-tachometer-alt" aria-hidden="true"></i>
     <p> Nossa Blast WA <i class="fas fa-angle-left right" aria-hidden="true"></i>
